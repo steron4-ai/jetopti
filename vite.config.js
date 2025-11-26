df54a48901b094b87a893623b050916b385633af
Ronny <ronny@jetopti.com>
2025-11-26 16:36:08 +0100

    Fix vite config

--- vite.config.js
+++ vite.config.js
@@ -4,43 +4,25 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    // Optimierungen für kleinere Bundles
+    // Cache Busting mit Hashes (WICHTIGSTER Teil!)
     rollupOptions: {
       output: {
-        // Cache Busting mit Hashes
         entryFileNames: 'assets/[name]-[hash].js',
         chunkFileNames: 'assets/[name]-[hash].js',
         assetFileNames: 'assets/[name]-[hash].[ext]',
         
         // Code Splitting für kleinere Dateien
         manualChunks: {
-          // React & React-DOM in separate Datei
           'react-vendor': ['react', 'react-dom', 'react-router-dom'],
-          
-          // Supabase in separate Datei
           'supabase-vendor': ['@supabase/supabase-js'],
-          
-          // Mapbox in separate Datei (falls verwendet)
-          'mapbox-vendor': ['mapbox-gl'],
         }
       }
     },
     
-    // Chunk Size Warnings
-    chunkSizeWarningLimit: 600, // Warnung bei >600kb
+    // Standard Minification (ohne terser)
+    minify: 'esbuild', // Standard, kein Extra Package nötig!
     
-    // Minification
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true, // Console.logs in Production entfernen
-        drop_debugger: true
-      }
-    }
-  },
-  
-  // Optimization
-  optimizeDeps: {
-    include: ['react', 'react-dom', 'react-router-dom']
+    // Chunk Size Warning
+    chunkSizeWarningLimit: 600
   }
 })
